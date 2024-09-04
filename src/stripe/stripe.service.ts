6b5a40591bf5ab55623e5992388fd09ac3033ca6
Karl OELSCHLAGER <oelschlager.karl@gmail.com>
2024-09-04 11:27:50 +0200

    Factures

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -1,6 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Stripe from 'stripe';
+import * as path from 'path';
+import * as fs from 'fs';
+import axios from 'axios';
+
 
 @Injectable()
 export class StripeService {
@@ -34,7 +38,14 @@ export class StripeService {
   }
 
 
-  async createPaymentIntent(email: string, name: string, amount: number, currency : string, paymentMethodId: string, return_url: string) {
+  async createPaymentIntent(
+    email: string,
+    name: string,
+    amount: number,
+    currency: string,
+    paymentMethodId: string,
+    return_url: string,
+  ) {
     // Recherchez le client existant par email
     const existingCustomers = await this.stripe.customers.list({
       email,
@@ -48,7 +59,7 @@ export class StripeService {
       customer = await this.createCustomer(email, name);
     }
 
-    // Créez un paiementIntent
+    // Créez un PaymentIntent
     const paymentIntent = await this.stripe.paymentIntents.create({
       amount,
       currency,
@@ -57,11 +68,70 @@ export class StripeService {
       confirm: true,
       return_url: return_url,
     });
+
+    // Vérifiez si le PaymentIntent est lié à une facture
+    let invoiceId = null;
+    if (paymentIntent.invoice) {
+      invoiceId = paymentIntent.invoice;
+    } else if (paymentIntent.latest_charge) {
+      const charge = await this.stripe.charges.retrieve(
+        paymentIntent.latest_charge as string,
+      );
+      if (charge.invoice) {
+        invoiceId = charge.invoice;
+      }
+    }
+
+    if (invoiceId) {
+      // Si une facture est trouvée, téléchargez et enregistrez-la en PDF
+      await this.saveInvoiceAsPdf(invoiceId);
+    }
+
     return {
       clientSecret: paymentIntent.client_secret,
+      invoiceId: invoiceId,
     };
   }
   
+  async saveInvoiceAsPdf(invoiceId: string) {
+    try {
+      // Récupérer les détails de la facture
+      const invoice = await this.stripe.invoices.retrieve(invoiceId);
+
+      if (!invoice.invoice_pdf) {
+        throw new Error('No PDF URL found for the invoice.');
+      }
+
+      const pdfUrl = invoice.invoice_pdf;
+
+      // Chemin d'enregistrement local du fichier PDF
+      const pdfPath = path.join(__dirname, `../invoices/${invoiceId}.pdf`);
+
+      // Télécharger le PDF avec Axios
+      const response = await axios({
+        url: pdfUrl,
+        method: 'GET',
+        responseType: 'stream',
+      });
+
+      // Créez un flux d'écriture pour enregistrer le fichier en local
+      const writer = fs.createWriteStream(pdfPath);
+
+      if (response.data) {
+        response.data.pipe(writer);
+      }
+
+      return new Promise<void>((resolve, reject) => {
+        writer.on('finish', resolve);
+        writer.on('error', reject);
+      });
+    } catch (error) {
+      throw new HttpException(
+        `Failed to save invoice PDF: ${error.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
 
     async createNewPriceForProduct(productId: string, amount: number, interval: 'month' | 'year' | null) {
       if (interval) {
